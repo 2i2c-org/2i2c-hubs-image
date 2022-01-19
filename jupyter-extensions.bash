1cbaa748502ad0fc5b401529385afec3d0548a0a
Shawn Wiggins <22923434+shawnwiggins@users.noreply.github.com>
2022-01-19 08:48:49 -0800

    Update jupyter-extensions.bash

--- jupyter-extensions.bash
+++ jupyter-extensions.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Install jupyter-contrib-nbextensions
-jupyter contrib nbextension install --user
+jupyter contrib nbextension install --sys-prefix
+
 
 # Configure spellchecker and toc2
-echo '{\n"load_extensions": {\n"spellchecker/main": true,\n"toc2/main": true,\n"nbextensions_configurator/config_menu/main": false,\n"contrib_nbextensions_help_item/main": false\n},\n"toc2": {\n"widenNotebook": false,\n"moveMenuLeft": false,\n"number_sections": false,\n"toc_window_display": false\n}\n}' > ~/.jupyter/nbconfig/notebook.json
+#echo '{\n"load_extensions": {\n"spellchecker/main": true,\n"toc2/main": true,\n"nbextensions_configurator/config_menu/main": false,\n"contrib_nbextensions_help_item/main": false\n},\n"toc2": {\n"widenNotebook": false,\n"moveMenuLeft": false,\n"number_sections": false,\n"toc_window_display": false\n}\n}' > ~/.jupyter/nbconfig/notebook.json
