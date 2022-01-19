47bd6a127ffc2f1df82790ca5be3c0a85f44ad04
Shawn Wiggins <22923434+shawnwiggins@users.noreply.github.com>
2022-01-19 09:01:21 -0800

    Delete jupyter-extensions.bash

--- jupyter-extensions.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install jupyter-contrib-nbextensions
-jupyter contrib nbextension install --sys-prefix
-
-
-# Configure spellchecker and toc2
-#echo '{\n"load_extensions": {\n"spellchecker/main": true,\n"toc2/main": true,\n"nbextensions_configurator/config_menu/main": false,\n"contrib_nbextensions_help_item/main": false\n},\n"toc2": {\n"widenNotebook": false,\n"moveMenuLeft": false,\n"number_sections": false,\n"toc_window_display": false\n}\n}' > ~/.jupyter/nbconfig/notebook.json
